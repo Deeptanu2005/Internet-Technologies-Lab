5309abbc2e20cdb689f3ae36831218e9f2ecf1d8
Deeptanu Sen <deeptanusends2005@icloud.com>
2024-11-07 13:33:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.over-container').addEventListener('scroll', function() {
+    const scrollTop = this.scrollTop;
+    const headerHeight = document.getElementsByTagName('h1')[0].clientHeight;
+    const totalHeaderHeight = headerHeight + 20;
+
+    if (scrollTop > totalHeaderHeight) {
+        document.querySelector('.navigation').classList.add('fixed-nav');
+        document.getElementById('desc').style.marginTop = (document.querySelector('.navigation').clientHeight + 80 + 20) +'px';
+    } else {
+        document.querySelector('.navigation').classList.remove('fixed-nav');
+        document.getElementById('desc').style.marginTop = '80px';
+    }
+});
+

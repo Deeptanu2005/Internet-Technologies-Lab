2204336322dcbc5c5da47dc9bc72d45f30cb2836
Deeptanu2005 <deeptanusends2005@icloud.com>
2024-11-07 21:24:46 +0530

    adding question 2

--- Q2/q2.js
+++ Q2/q2.js
@@ -1,39 +1,40 @@
 document.getElementById('result').value = 'Result';
 
 document.getElementById('getMax').addEventListener('click', () => {
-    let a = document.getElementById('first').value;
-    let b = document.getElementById('second').value;
-    let c = document.getElementById('third').value;
+    let a = document.getElementById('first').value.trim();
+    let b = document.getElementById('second').value.trim();
+    let c = document.getElementById('third').value.trim();
 
-    if (a.trim() === "" || b.trim() === "" || c.trim() === "") {
+    if (a === "" || b === "" || c === "") {
         document.getElementById('result').value = 'All fields must be filled';
     } else {
         a = Number(a);
         b = Number(b);
         c = Number(c);
 
-        let max;
-
-        // max = Math.max(a, b, c); // using Math
-
-        if (a > b) {
-            if (a > c) {
-                max = a;
-            } else {
-                max = c;
-            }
-        } else {
-            if (b > c) {
-                max = b;
-            } else {
-                max = c;
-            }
-        }
-
-        if (isNaN(max)) {
+        if (isNaN(a) || isNaN(b) || isNaN(c)) {
             document.getElementById('result').value = 'Invalid Input';
         } else {
-            document.getElementById('result').value = 'Maximum is = ' + max;
+
+            let max;
+
+            // max = Math.max(a, b, c); // using Math
+
+            if (a > b) {
+                if (a > c) {
+                    max = a;
+                } else {
+                    max = c;
+                }
+            } else {
+                if (b > c) {
+                    max = b;
+                } else {
+                    max = c;
+                }
+            }
+
+            document.getElementById('result').value = `Maximum is =  ${max}`;
         }
     }
 });
